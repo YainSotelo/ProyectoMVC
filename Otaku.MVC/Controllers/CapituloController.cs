ef17d7eab20272fab747dab0d411469f2661f347
YainSotelo <yainsotelo3@gmail.com>
2022-02-14 17:06:10 -0300

    QUE LINDA QUE ESTAS HDPPP

--- Otaku.MVC/Controllers/CapituloController.cs
+++ Otaku.MVC/Controllers/CapituloController.cs
@@ -5,12 +5,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Otaku.MVC.ViewModels;
 using Otaku.MVC.Models;
+using Otaku.Core;
 
 namespace Otaku.MVC.Controllers
 {
     public class CapituloController : Controller
     {
-        
+        [HttpGet]
+        public IActionResult Index()
+            => View(Repositorio.Capitulos);
+
+        [HttpGet]
+        public IActionResult Capitulos(int id)
+        {
+            var Capitulo = Repositorio.GetCapitulo(id);
+            if (Capitulo is null)
+            {
+                return NotFound();
+            }
+            return View(Capitulo);
+        }
+
+        [HttpGet]
+        public IActionResult FormAlta() => View();
+
+        [HttpPost]
+        public IActionResult FormAlta(Capitulo Capitulo)
+        {
+            Repositorio.AgregarCapitulo(Capitulo);
+            return View("Index", Repositorio.Capitulos);
+        }
     }
 }
